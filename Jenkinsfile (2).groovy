f6d15b4e874f3adb5a96124f43cd0f60e5b3e66b
ambrosh04 <162113200+ambrosh04@users.noreply.github.com>
2024-06-20 16:07:02 +0530

    Add files via upload

---
+++ Jenkinsfile (2).groovy
@@ -0,0 +1,23 @@
+#Jenkinsfile
+pipeline {
+    agent any 
+    stages {
+        stage('Build') { 
+            steps {
+                echo "This is Build stage."
+                sh 'sudo docker build -t todo-list .'
+            }
+        }
+        stage('Test') { 
+            steps {
+                echo "This is Test stage." 
+                sh 'sudo docker run -d -p 8000:8000 todo-list'
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo "Successful" 
+            }
+        }
+    }
+}
