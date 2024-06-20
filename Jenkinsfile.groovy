0de9e27dd214f492709d03d5f5122ba71e452a66
ambrosh04 <162113200+ambrosh04@users.noreply.github.com>
2024-06-20 16:22:31 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,13 +4,13 @@ pipeline {
         stage('Build') { 
             steps {
                 echo "This is Build stage."
-                sh 'sudo docker build -t todo-list .'
+                sh 'docker build -t todo-list .'
             }
         }
         stage('Test') { 
             steps {
                 echo "This is Test stage." 
-                sh 'sudo docker run -d -p 8000:8000 todo-list'
+                sh 'docker run -d -p 8000:8000 todo-list'
             }
         }
         stage('Deploy') { 
