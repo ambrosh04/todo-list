194ad8e4da3567e4191dc7ca555c199b00be7aa4
ambrosh04 <162113200+ambrosh04@users.noreply.github.com>
2024-08-05 15:28:52 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
         ECR_REPOSITORY = 'public.ecr.aws/l6s9i6b7/todo-list'
         IMAGE_TAG = 'latest'
         CLUSTER_NAME = 'my-cluster'
-        SERVICE_NAME = 'my-service'
+        SERVICE_NAME = 'todo-list-deployment'
         CONTAINER_NAME = 'my-container'
         KUBECONFIG = '/var/lib/jenkins/.kube/config'
         AWS_CREDENTIALS = 'aws-credentials-id'
